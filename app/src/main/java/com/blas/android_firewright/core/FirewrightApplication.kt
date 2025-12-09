417bec8c1c757fe29f8a1560282acd20bb34bb1d
Blasius <blasius@Owners-MacBook-Pro.local>
2025-12-09 13:13:46 +0700

    Initial Android project setup

---
+++ app/src/main/java/com/blas/android_firewright/core/FirewrightApplication.kt
@@ -0,0 +1,7 @@
+package com.blas.android_firewright.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FirewrightApplication : Application()
---
+++ app/src/main/java/com/blas/android_firewright/core/di/AppModule.kt
@@ -0,0 +1,16 @@
+package com.blas.android_firewright.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWelcomeText(): String = "Hello from Hilt"
+}
